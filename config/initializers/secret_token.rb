bb6325199c65dd849a73646ee46de6244c10b095
Sophiarith <sophiarith@sophiarith-Inspiron-N4030.(none)>
2012-09-22 20:23:13 +0700

    add rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Responsive::Application.config.secret_token = 'fe983c5af378a3c702d625f6fbea9bbc6af5a28ea2aeff78f041b3cdfb1ace72ef9b4ee6393c5ea23310ead961f22ac8399a82ca10f1f3ec031d31ffdf4bea06'
