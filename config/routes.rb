705dec3081d57a5cc09a232b34a593cd66c152ec
Sophiarith <sophiarith@sophiarith-Inspiron-N4030.(none)>
2012-09-23 07:50:19 +0700

    rake asset

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Responsive::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  root :to => 'homes#index'
+  resources :homes
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
